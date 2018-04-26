da9720bae5b41ca0d946f909aa739d714826e484
weresquirrel <vermokuss@gmail.com>
2018-04-26 14:57:36 +0100

    my version after 4hours 15mins total

--- app.js
+++ app.js
@@ -1,8 +1,43 @@
-console.log('up N ...');
 
 $(() => {
 
-  console.log('... running');
+  const $menu = $('button');
+  const $nav = $('nav');
+  const $body = $('body');
+  const $house = $('.house');
+  const $beach = $('.beach');
+  const $stud = $('.stud');
+
+  $menu.on('click', function() {
+    $menu.toggleClass('hamburger cross');
+    $nav.toggleClass('off on');
+  });
+
+  $house.hover(function() {
+    $body.removeClass();
+    $body.addClass('house-bg');
+    $body.addClass('white');
+    // console.log('on');
+  }, function() {
+    $body.removeClass();
+    // console.log('off');
+  });
+
+  $beach.hover(function() {
+    $body.removeClass();
+    $body.addClass('beach-bg');
+    $body.addClass('white');
+  }, function() {
+    $body.removeClass();
+  });
+
+  $stud.hover(function() {
+    $body.removeClass();
+    $body.addClass('stud-bg');
+    $body.addClass('white');
+  }, function() {
+    $body.removeClass();
+  });
 
 
 //////
